32adec163a742a6c7a6d62093bd0c1bb54804662
VadymMytr <42971417+VadymMytr@users.noreply.github.com>
2019-05-18 21:14:12 +0300

    Реализован класс Internship. Метод SetInterns() выполняет все действия, связанные с отбором студентов, а именно: вызывает метод AverageNote, который возвращает средний уровень знаний всех студентов; вызывает метод CheckStudents(), который проверяет, больше ли у потенциального интерна баллов, чем средний. Если да - этот студент добавляется в List интернов с помощью метода AddIntern().

--- Institution/InterLink/Internship.cs
+++ Institution/InterLink/Internship.cs
@@ -1,16 +1,63 @@
+using System;
+using InternshipTest.Person;
+using System.Collections.Generic;
+
 namespace InternshipTest.Institution.InterLink
 {
     public class Internship
     {
+        private List<Student> Interns = new List<Student>(); 
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            private set
+            {
+                if(value != null && value.Length > 0)
+                    Name = value;
+            }
+        }
         public Internship(string name)
         {
-            //TODO: Implementation is needed      
+            Name = name;      
         }
-
         public string GetStudents()
+        {   
+            string answer = "";
+            foreach(Student intern in Interns)
+            {
+                answer += intern.Name + "\n";
+            }        
+            return answer;
+        }
+        private void SetInterns()
+        {            
+            List<Student> allStudents =  University.GetStudentList();            
+            double avg = AverageNote(allStudents);
+            CheckStudents(allStudents, avg);
+        }
+        private double AverageNote(List<Student> students)
         {
-            //TODO: Implementation is needed
-            return "Andrew Maslenko\nJulia Veselkina\n";
+            double answer = 0;
+            for(int i = 0; i < students.Count; i++)            
+                answer += Convert.ToDouble(students[i].knowledgeLevel);
+            answer /= students.Count;
+            return answer - answer % 0.1;
+        }
+        private void CheckStudents(List<Student> students, double avgNote)
+        {
+            foreach(Student stud in students)
+            {
+                Knowledge studKnowledge = stud.knowledgeLevel;
+                if(studKnowledge.Level > avgNote)
+                    AddIntern(stud);
+            }
+        }
+        private void AddIntern(Student intern)
+        {
+            Interns.Add(intern);
         }
     }
 }
