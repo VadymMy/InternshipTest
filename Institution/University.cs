a344972075127ce5ac279a71ab1f7b31715114d6
VadymMytr <42971417+VadymMytr@users.noreply.github.com>
2019-05-18 17:04:02 +0300

    Описан класс University. Обьект University имеет свойство Name, а так же список студентов, которые там учатся. Метод GetStudentList возвращает этот список.

--- Institution/University.cs
+++ Institution/University.cs
@@ -1,17 +1,34 @@
 using InternshipTest.Person;
+using System.Collections.Generic;
 
 namespace InternshipTest.Institution
 {
     public class University
     {
+        private List<Student> Students = new List<Student>();
+        public List<Student> GetStudentList()
+        {
+            return Students;
+        }
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            private set
+            {
+                if(value != null && value.Length > 0)
+                    Name = value; 
+            }
+        }
         public University(string name)
         {
-            //TODO: Implementation is needed  
+            Name = name;
         }
-
         public void AddStudent(Student student)
         {
-            //TODO: Implementation is needed
-        }
+            Students.Add(student);
+        }        
     }
 }
